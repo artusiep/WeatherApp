eae574b4644abdfdfc620d218f205872667f916d
Artur Siepietowski <artursiepietowski@gmail.com>
2018-10-21 22:21:18 +0200

    Created main storyboard

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -10,11 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var cityTextField: UITextField!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var statusImageView: UIImageView!
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var maxTempTextField: UITextField!
+    @IBOutlet weak var minTempTextField: UITextField!
+    @IBOutlet weak var windVelTextField: UITextField!
+    @IBOutlet weak var windDirTextField: UITextField!
+    @IBOutlet weak var rainfallTextField: UITextField!
+    @IBOutlet weak var pressureTextField: UITextField!
+    @IBOutlet weak var prevBtn: UIButton!
+    @IBOutlet weak var nextBtn: UIButton!
+    var date = Date()
+    let calendar = Calendar.current
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        bootstrap()
+    }
+    
+    func bootstrap() {
+        dateLabel.text = date.description
     }
 
+    
+    func changeDate(day: Int) {
+        date = calendar.date(byAdding: .day, value: day, to: date)!
+    }
+    
+    
+    
 
+    @IBAction func nextDate(_ sender: Any) {
+        changeDate(day: 1)
+        dateLabel.text = date.description
+    }
+    
+    @IBAction func prevDate(_ sender: Any) {
+        changeDate(day: -1)
+        dateLabel.text = date.description
+    }
 }
 
